1131f2ea4e35a27f29c1ef182ec9e2849a5a8222
Ankit Kumar <kumarankit12353@gmail.com>
2022-08-06 23:23:38 +0530

    diffutils logics improved

--- features/pull_requests/src/main/java/lazycoder21/droid/pull_requests/presentation/adapter/PullRequestAdapterDiffUtil.kt
+++ features/pull_requests/src/main/java/lazycoder21/droid/pull_requests/presentation/adapter/PullRequestAdapterDiffUtil.kt
@@ -1,14 +1,21 @@
 package lazycoder21.droid.pull_requests.presentation.adapter
 
 import androidx.recyclerview.widget.DiffUtil
+import lazycoder21.droid.pull_requests.domain.model.PullRequest
 import lazycoder21.droid.pull_requests.presentation.adapter.base.BaseItemModel
 
 object PullRequestAdapterDiffUtil : DiffUtil.ItemCallback<BaseItemModel>() {
     override fun areItemsTheSame(oldItem: BaseItemModel, newItem: BaseItemModel): Boolean {
-        return false
+        return when {
+            oldItem is PullRequest && newItem is PullRequest -> oldItem === newItem
+            else -> false
+        }
     }
 
     override fun areContentsTheSame(oldItem: BaseItemModel, newItem: BaseItemModel): Boolean {
-        return false
+        return when {
+            oldItem is PullRequest && newItem is PullRequest -> oldItem == newItem
+            else -> false
+        }
     }
 }
